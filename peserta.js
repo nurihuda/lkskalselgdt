042b6c7ff03cab8bf64813593d171b8287ffaee7
nurihuda <nurih789@gmail.com>
2026-06-08 00:35:51 +0700

    Add peserta.js to store competition participant data
    This file contains participant data for the competition.

---
+++ peserta.js
@@ -0,0 +1,18 @@
+// File: peserta.js
+// Tempat menyimpan data seluruh peserta lomba
+const DATA_PESERTA = [
+    {
+        no: "99",
+        nama: "Muhamad Nuri Huda",
+        link: "https://drive.google.com/drive/folders/1owsL3gQQ-G88eXH7moBHe90f7F1Uxhqy"
+    },
+    {
+        no: "98",
+        nama: "Muhamad Nuri",
+        link: "https://drive.google.com/drive/folders/1mEagiaQJd0JTopKPyvkpKau2oMSFIPLq"
+    }
+    // Anda bisa menambah peserta baru di bawah ini dengan format yang sama
+];
+
+// Ekspor agar bisa digunakan di script.js
+window.DATA_PESERTA = DATA_PESERTA;
