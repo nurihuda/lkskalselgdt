dc616a5d597179aab31ee9dddce8926d378d7dd8
nurihuda <nurih789@gmail.com>
2026-06-08 23:06:54 +0700

    Update participant data in peserta.js
    Replaced existing participant data with a new list of participants for the competition.

--- peserta.js
+++ peserta.js
@@ -1,18 +1,98 @@
-// File: peserta.js
-// Tempat menyimpan data seluruh peserta lomba
-const DATA_PESERTA = [
-    {
-        no: "99",
-        nama: "Muhamad Nuri Huda",
-        link: "https://drive.google.com/drive/folders/1owsL3gQQ-G88eXH7moBHe90f7F1Uxhqy"
-    },
-    {
-        no: "98",
-        nama: "Muhamad Nuri",
-        link: "https://drive.google.com/drive/folders/1mEagiaQJd0JTopKPyvkpKau2oMSFIPLq"
-    }
-    // Anda bisa menambah peserta baru di bawah ini dengan format yang sama
+// Salin dan gantikan isi di file peserta.js dengan data di bawah ini
+window.DATA_PESERTA = [
+  {
+    "no": "21",
+    "nama": "Muhammad Ilham Al hadi",
+    "link": "https://drive.google.com/drive/folders/1RxDufk-9vZzcplQ8ZOBZ3GHO-4Q5-E8Z"
+  },
+  {
+    "no": "15",
+    "nama": "Muhammad Zalfa Al Fakhry",
+    "link": "https://drive.google.com/drive/folders/1jTyBBpxEt6KxgO8PRn17N2TQ9yQIYmVN"
+  },
+  {
+    "no": "03",
+    "nama": "Aprilia Kamila",
+    "link": "https://drive.google.com/drive/folders/1oVBrLritFBugOS7eOvpXQslsRdg4w7SP"
+  },
+  {
+    "no": "16",
+    "nama": "Muhammad Yoga Permana",
+    "link": "https://drive.google.com/drive/folders/1gV2Nruot7fO81bl7-4RpwqCfTNpkUeL2"
+  },
+  {
+    "no": "14",
+    "nama": "Siti Fatimah",
+    "link": "https://drive.google.com/drive/folders/1ToSKN5UVJiP3Q16xLG491KhqVuYNALyz"
+  },
+  {
+    "no": "09",
+    "nama": "Ahmad Nawawi",
+    "link": "https://drive.google.com/drive/folders/16WHJ8K3B8quQC62JMC4NGfHGDKbKM9i6"
+  },
+  {
+    "no": "20",
+    "nama": "Nur Intania Apriliani",
+    "link": "https://drive.google.com/drive/folders/1z-y_5LKk2-XxgY_xSuLxbqO2mVpZ4Efc"
+  },
+  {
+    "no": "12",
+    "nama": "Rizki Fadilah",
+    "link": "https://drive.google.com/drive/folders/1sVJ8LQaJqVLzcpCj1GZCybpyTs3N8ZVJ"
+  },
+  {
+    "no": "22",
+    "nama": "Muhammad Fathurroyyan",
+    "link": "https://drive.google.com/drive/folders/1OQOZqyfF9YwKQhqYgUTn6O_AX5VuzeUt"
+  },
+  {
+    "no": "5",
+    "nama": "Miftahul Jannah",
+    "link": "https://drive.google.com/drive/folders/1QmdyDPXTQdvMIZR1QKaGzgflo_egz81v"
+  },
+  {
+    "no": "11",
+    "nama": "Citra Dyah Ariyanti",
+    "link": "https://drive.google.com/drive/folders/1ymEDfrp-Gn4vggBJnlfxJ3NSDvdKWuK"
+  },
+  {
+    "no": "17",
+    "nama": "Muhammad Redza Ar raffi",
+    "link": "https://drive.google.com/drive/folders/1m2SpBw164aklhnQYf3NvZw24-lbSx4nB"
+  },
+  {
+    "no": "07",
+    "nama": "AKHMAD HAFIZ ANSYARI",
+    "link": "https://drive.google.com/drive/folders/1z8o3SRHzayKt1QFkWaggkkiWHnVoKWe"
+  },
+  {
+    "no": "18",
+    "nama": "Ivena laila khoiriyah",
+    "link": "https://drive.google.com/drive/folders/1LdrOmbHhl7v8gf_OhCO8xnHmTcfs4735"
+  },
+  {
+    "no": "13",
+    "nama": "Talitha Tarani",
+    "link": "https://drive.google.com/drive/folders/1wX6AIrYwmyOUYPSrv4bcK_--G_25uqNU"
+  },
+  {
+    "no": "8",
+    "nama": "Aulia rahman",
+    "link": "https://drive.google.com/drive/folders/1DRm6_hy4dvgBPy3poT5yjki1uGENKccA"
+  },
+  {
+    "no": "24",
+    "nama": "Muhammad Nabiel Fatahillah",
+    "link": "https://drive.google.com/drive/folders/1wdXDY4XtCoawmMCy6QVLrp51NQOIvu17"
+  },
+  {
+    "no": "10",
+    "nama": "AHMAD",
+    "link": "https://drive.google.com/drive/folders/1-f4bLRbkx-dx_wgEpSSJAF42K-gyt8uz"
+  },
+  {
+    "no": "02",
+    "nama": "Zahwa Alya",
+    "link": "https://drive.google.com/drive/folders/1s4JlXnrfczJSkaZOo7lauW3KSenVp8ju"
+  }
 ];
-
-// Ekspor agar bisa digunakan di script.js
-window.DATA_PESERTA = DATA_PESERTA;
